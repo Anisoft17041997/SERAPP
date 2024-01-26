fb3225415aa783b2c0e454550b111ed13fdebee0
AGBONON EDAGBEDJI Yao Anicet <lanicet17@gmail.com>
2024-01-26 16:42:04 +0100

    code clean and API update

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import librosa
+import random as r
 import numpy as np
 from flask_cors import CORS
 from main_telegram_notif import notify
@@ -10,23 +11,28 @@ app = Flask(__name__)
 CORS(app)
 #
 # Load the machine learning model
-model = joblib.load('model.pkl')
+model = joblib.load('dtc_model.pkl')
 #
 #
-def extract_features(normalized_y, sr, max_len=100):
-    mfccs = librosa.feature.mfcc(y=normalized_y, sr=sr, n_mfcc=13)
-    chroma = librosa.feature.chroma_stft(y=normalized_y, sr=sr)
-    spectral_contrast = librosa.feature.spectral_contrast(y=normalized_y, sr=sr)
+def process_audio(file_path):
+    y, sr = librosa.load(file_path)
+    y, _ = librosa.effects.trim(y)
+    normalized_y = librosa.util.normalize(y)
 
-    mfccs = librosa.util.fix_length(mfccs, size = max_len, axis=1)
-    chroma = librosa.util.fix_length(chroma, size = max_len, axis=1)
-    spectral_contrast = librosa.util.fix_length(spectral_contrast, size = max_len, axis=1)
+    return normalized_y, sr
+#
+#
+def extract_features_seven_features(data, sample_rate):
+    zcr = np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
+    rms_energy = np.mean((librosa.feature.rms(y=data)).T, axis=0)
+    spectral_centroid = np.mean(librosa.feature.spectral_centroid(y=data, sr=sample_rate).T, axis=0)
+    spectral_spread = np.mean(librosa.feature.spectral_bandwidth(y=data, sr=sample_rate).T, axis=0)
+    spectral_flux = np.mean(librosa.onset.onset_strength(y=data, sr=sample_rate).T, axis=0)
+    mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate, n_mfcc=13).T, axis=0)
+    chroma = np.mean(librosa.feature.chroma_stft(y=data, sr=sample_rate).T, axis=0)
 
-    flat_mfccs = np.ravel(mfccs)
-    flat_chroma = np.ravel(chroma)
-    flat_spectral_contrast = np.ravel(spectral_contrast)
-
-    return np.array(flat_mfccs).reshape(1, -1), np.array(flat_chroma).reshape(1, -1), np.array(flat_spectral_contrast).reshape(1, -1)
+    # Stack all features horizontally
+    return zcr, rms_energy, spectral_centroid, spectral_spread, spectral_flux, mfcc, chroma
 #
 #
 @app.route('/predict', methods=['POST'])
@@ -39,25 +45,24 @@ def predict():
             audio_file = request.files['audio_file']
             audio_file.save('./audio.wav')
 
-            # Load the audio file using librosa
-            audio, sr = librosa.load('./audio.wav', sr=None)
-            audio, _ = librosa.effects.trim(audio)
-            normalized_y = librosa.util.normalize(audio)
+            # Process the audio
+            normalized_y, sr = process_audio('./audio.wav')
 
             # Extract features from the audio
-            flat_mfccs, flat_chroma, flat_spectral_contrast = extract_features(normalized_y, sr)
-            features = np.concatenate((flat_mfccs, flat_chroma, flat_spectral_contrast), axis=1)
+            features = np.hstack((extract_features_seven_features(normalized_y, sr)))
+            features = features.reshape(1, -1)
             
             # Make a prediction using the loaded model
             prediction = model.predict(features)[0]
 
             # Notify the angry prediction
-            notify("DET001") if prediction == 1 else None
+            notify(f'DET{int(r.uniform(100, 999))}') if prediction == 1 else None
 
             # Return the prediction as a JSON response
             return jsonify({'prediction': str(prediction)})
     except Exception as e:
         return jsonify({'error': str(e)})
-   
+#
+#
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
